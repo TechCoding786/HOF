8efad4b5ae181af13040264b207c19b846d8df3d
TechCoding786 <119719675+TechCoding786@users.noreply.github.com>
2024-02-20 04:30:01 -0400

    second question prectice

--- problem.js
+++ problem.js
@@ -16,3 +16,20 @@ const output =arr?.map(function (x){
 
 
 console.log("--->",output);
+
+//Given an array of objects, write a function that returns a new array with only the objects whose “completed” property is true.
+
+const test1 = [
+    {name : 'AS01' , completed : true},
+    {name : 'AS02' , completed : false},
+    {name : 'AS03' , completed : true},
+    {name : 'AS04' , completed : false},
+    {name : 'AS05' , completed : true},
+    {name : 'AS06' , completed : false},
+];
+
+// const output1 = test1?.filter((item) => item?.completed);
+// console.log('---->' , output1);
+
+const output1 = test1?.filter((x)=>x?.completed);
+console.log("--->",output1);
